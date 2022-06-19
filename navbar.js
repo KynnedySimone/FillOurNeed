c6de79225743ddac0d6ead4237cbb483fc1f1ca6
Glitch (inconclusive-gorgeous-rover) <none>
2022-06-19 15:28:20 +0000

    🚸🧈 Updated with Glitch

---
+++ navbar.js
@@ -0,0 +1,20 @@
+  <nav class="navbar is-transparent">
+    <div class="navbar-brand">
+      <img class= "image is-128x128"
+          src="https://cdn.glitch.global/c0d8b101-0093-4160-b894-f0d8add6e0e6/thumbnails%2F9fa0350c-bfd8-4f97-94b8-65497cfbe38a.My%20project%20(4)_adobe_express.png?1655638091548"/>
+      <a class="navbar-item" href="https://bulma.io"> </a>
+      <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/community.html"> Community </a>
+        <a class="navbar-item" href="/missions.html"> Missions </a>
+        <a class="navbar-item" href="/profile.html"> Profile </a>
+      </div>
+    </div>
+  </nav>
